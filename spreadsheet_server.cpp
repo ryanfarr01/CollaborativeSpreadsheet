a4d408695d31d91406ca9a803c1a10a443681391
Alienware PC Ryan or Jordan <interactivedeli@gmail.com>
2015-04-05 14:31:40 -0600

    I just put in a couple things so that we have somewhere to start from

---
+++ spreadsheet_server.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+typedef unsigned char BYTE;
+
+int main(int argc, char** args)
+{
+  
+  return 0;
+}
+
+string ConvertMessage(int length, BYTE * arr)
+{
+  
+}
